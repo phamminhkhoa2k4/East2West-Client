d2f94138af567e7c9339d6b599c8fb6466833ead
pham minh khoa <khoapham280@gmail.com>
2024-08-01 10:58:19 +0700

    💄 style : style sign in page and sign up page

---
+++ components/Auth/Register/SignUp.tsx
@@ -0,0 +1,34 @@
+"use client";
+import Link from "next/link";
+import React from "react";
+import GoogleSigninButton from "../GoogleSigninButton";
+import SignUpForm from "../SignUpForm";
+
+export default function SignUp() {
+  return (
+    <>
+      <GoogleSigninButton text="Sign in" />
+
+      <div className="my-6 flex items-center justify-center">
+        <span className="block h-px w-full bg-stroke "></span>
+        <div className="block w-full min-w-fit bg-white px-3 text-center font-medium ">
+          Or sign up with email
+        </div>
+        <span className="block h-px w-full bg-stroke"></span>
+      </div>
+
+      <div>
+        <SignUpForm />
+      </div>
+
+      <div className="mt-6 text-center">
+        <p>
+          Already have an account{" "}
+          <Link href="/signin" className="text-primary">
+            Sign In
+          </Link>
+        </p>
+      </div>
+    </>
+  );
+}
