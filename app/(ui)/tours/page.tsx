c87a7f23565ca7e8ae3e0ad51ea625f9348d57b0
pham minh khoa <khoapham280@gmail.com>
2024-08-18 17:25:06 +0700

    💄 style : update tour

--- app/(ui)/tours/page.tsx
+++ app/(ui)/tours/page.tsx
@@ -1,9 +1,34 @@
+import Banner from "@/components/Banner/Banner";
+import Breadcrumb from "@/components/Breadcrumbs/Breadcrumbs";
+import CarouselFeatures from "@/components/CarouselFeatures";
+import Categories from "@/components/Categories";
+import OtherOffers from "@/components/OtherOffers";
+import Packages from "@/components/tour/Packages";
+import Recently from "@/components/tour/Recently";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Tours Package | East2West",
+  description: "This Is Tours Package Page Of East2West Tours and Travel",
+};
+
 
 
 const Tours = () => {
   return (
     <>
-      <h1>This is Tours</h1>
+      <Banner url={"/banner/tour_banner.jpeg"} />
+      <div className="mx-20">
+        <Breadcrumb />
+      </div>
+      <div className="mx-40">
+        <Recently />
+        <CarouselFeatures />
+        <Packages />
+        <Packages />
+      </div>
+      {/* <Categories /> */}
+      {/* <OtherOffers /> */}
     </>
   );
 };
