871e5b4844c12d58ce9cbf36b09146bd2fc49422
pham minh khoa <khoapham280@gmail.com>
2024-08-20 23:34:18 +0700

    💄 style : style bathroom in host homestay page

---
+++ app/(ui)/homestays/host/bathroom/page.tsx
@@ -0,0 +1,208 @@
+"use client";
+import Image from "next/image";
+import { useState } from "react";
+
+const Bathroom = () => {
+  const [countAdult, setCountAdult] = useState<number>(0);
+  const [countChildren, SetCountChildren] = useState<number>(0);
+  const [countBaby, SetCountBaby] = useState<number>(0);
+  return (
+    <div>
+      <div className="bg-white fixed right-0 left-0 top-0  px-15 pt-5 pb-5 z-999 border-b">
+        <div className="flex items-center justify-between">
+          <div className="w-20 h-20">
+            <Image
+              src={"/Logo.png"}
+              alt=""
+              height={300}
+              width={300}
+              className="object-center object-cover w-full h-full"
+            />
+          </div>
+          <div>
+            <button className="border px-4 py-2 rounded-full">Exit</button>
+          </div>
+        </div>
+      </div>
+      <div className="flex flex-col items-center justify-center mt-36 mb-30">
+        <div className="text-4xl font-semibold w-[630px]">
+          Khách có thể sử dụng loại phòng tắm nào?
+        </div>
+        <div className="w-[630px] mt-5">
+          <div className="bg-white w-full border py-4 px-10 rounded-2xl shadow-md">
+            <div className="flex items-center w-full justify-between py-6 border-b-2">
+              <div>
+                <h3 className="font-medium text-lg">Chung</h3>
+                <span className="text-[#666]">
+                  Phòng tắm có đường đi đến phòng của khách và chỉ dành cho
+                  khách.
+                </span>
+              </div>
+              <div className="flex items-center gap-3">
+                <button
+                  className="border rounded-full p-2"
+                  onClick={() =>
+                    setCountAdult((prev) => (prev > 0 ? prev - 1 : prev))
+                  }
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke-width="1.5"
+                    stroke="currentColor"
+                    className="size-6"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      d="M5 12h14"
+                    />
+                  </svg>
+                </button>
+                <span>{countAdult}</span>
+                <button
+                  className="border rounded-full p-2"
+                  onClick={() => setCountAdult((prev) => prev + 1)}
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke-width="1.5"
+                    stroke="currentColor"
+                    className="size-6"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      d="M12 4.5v15m7.5-7.5h-15"
+                    />
+                  </svg>
+                </button>
+              </div>
+            </div>
+            <div className="flex items-center w-full justify-between py-6 border-b-2">
+              <div>
+                <h3 className="font-medium text-lg">Riêng và khép kín</h3>
+                <span className="text-[#666]">
+                  Phòng tắm có đường đi đến phòng của khách và chỉ dành cho
+                  khách.
+                </span>
+              </div>
+              <div className="flex items-center gap-3">
+                <button
+                  className="border rounded-full p-2"
+                  onClick={() => {
+                    SetCountChildren((prev) => (prev > 0 ? prev - 1 : prev));
+                  }}
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke-width="1.5"
+                    stroke="currentColor"
+                    className="size-6"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      d="M5 12h14"
+                    />
+                  </svg>
+                </button>
+                <span>{countChildren}</span>
+                <button
+                  className="border rounded-full p-2"
+                  onClick={() => {
+                    SetCountChildren((prev) => prev + 1);
+                  }}
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke-width="1.5"
+                    stroke="currentColor"
+                    className="size-6"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      d="M12 4.5v15m7.5-7.5h-15"
+                    />
+                  </svg>
+                </button>
+              </div>
+            </div>
+            <div className="flex items-center w-full justify-between py-6 ">
+              <div>
+                <h3 className="font-medium text-lg">Riêng</h3>
+                <span className="text-[#666]">
+                  Đó là không gian riêng nhưng cần đi qua khu vực chung, như
+                  hành lang, để vào được.
+                </span>
+              </div>
+              <div className="flex items-center gap-3">
+                <button
+                  className="border rounded-full p-2"
+                  onClick={() => {
+                    SetCountBaby((prev) => (prev > 0 ? prev - 1 : prev));
+                  }}
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke-width="1.5"
+                    stroke="currentColor"
+                    className="size-6"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      d="M5 12h14"
+                    />
+                  </svg>
+                </button>
+                <span>{countBaby}</span>
+                <button
+                  className="border rounded-full p-2"
+                  onClick={() => {
+                    SetCountBaby((prev) => prev + 1);
+                  }}
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke-width="1.5"
+                    stroke="currentColor"
+                    className="size-6"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      d="M12 4.5v15m7.5-7.5h-15"
+                    />
+                  </svg>
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className=" bg-white border-t-4 flex fixed left-0 right-0 bottom-0 items-center justify-between">
+        <button className="px-5 py-3 my-5 ml-5 rounded-xl text-lg font-bold text-white bg-slate-400">
+          Back
+        </button>
+        <button className="px-5 py-3 my-5 mr-5 rounded-xl text-lg font-bold text-white bg-blue-500">
+          Continue
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Bathroom;
