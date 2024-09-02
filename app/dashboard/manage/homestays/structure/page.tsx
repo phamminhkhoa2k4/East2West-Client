e36ab21dc45177b6f2c378d5e3af43fb98837144
pham minh khoa <khoapham280@gmail.com>
2024-09-02 12:29:15 +0700

    💄 style : manage structure

---
+++ app/dashboard/manage/homestays/structure/page.tsx
@@ -0,0 +1,52 @@
+"use client";
+import React, { useEffect, useState } from "react";
+import DefaultLayout from "@/components/Layouts/DefaultLayout";
+import CustomTable from "@/components/Tables/CustomTable";
+
+const columns = [
+  { key: "makeId", label: "ID" },
+  { key: "makeName", label: "Structure  Name" },
+];
+
+interface Make {
+  makeId: number;
+  makeName: string;
+}
+
+const Makes = () => {
+  const [data, setData] = useState<Make[]>([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("http://localhost:8080/api/cars/make");
+        const result: Make[] = await response.json();
+
+        const formattedData = result.map((make: Make) => ({
+          makeId: make.makeId,
+          makeName: make.makeName,
+        }));
+
+        setData(formattedData);
+      } catch (error) {
+        console.error("Error fetching makes:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <DefaultLayout>
+      <CustomTable
+        columns={columns}
+        data={data}
+        title="Structures"
+        createUrl="/dashboard/manage/cars/make/add"
+      />
+      <div></div>
+    </DefaultLayout>
+  );
+};
+
+export default Makes;
